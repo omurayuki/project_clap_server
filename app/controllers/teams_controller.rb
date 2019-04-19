88c2a4698c439626cb1d4a3661616ef55c7ea661
omurayuki <g1221438@icloud.com>
2019-04-19 12:31:39 +0900

    新規controller作成

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -24,16 +24,9 @@ class TeamsController < ApplicationController
     end
   end
 
-  
-
   private
 
   def team_params
     params.permit(:team_id, :name, :sports_id, :grade_id)
   end
 end
-
-#監督などはteamを編集できる機能
-#全userにteamIDとuserToken配ることでアプリを再現できる
-#sportsとgradeはstringで受け取る そしてそれぞれのtableで保存して, idをteamsに保存
-#userにはteamIdではなく、teamのIDを割り当てる
